ad77aa3d820858e65e6c78de16253c4bba5d7e80
carlosinti_94@hotmail.com <carlosinti_94@hotmail.com>
2025-10-30 15:54:32 +0100

    CPP07 - Ex01

---
+++ CPP07/ex01/src/iter.hpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cagonzal <cagonzal@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/30 14:58:10 by cagonzal          #+#    #+#             */
+/*   Updated: 2025/10/30 15:38:35 by cagonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include<iomanip>
+#include<iostream>
+
+class Tester
+{
+	private:
+		int _n;
+
+	public:
+		Tester(void) : _n(42) { return; }
+		Tester(int n) : _n(n) { return; }
+		int get_n() const { return this->_n; }
+};
+
+std::ostream &operator<< (std::ostream& os, Tester const &t)
+{
+	os << t.get_n();
+	return os;
+}
+
+template< typename T >
+void print( T &x )
+{
+    std::cout << x << std::endl;
+}
+
+template< typename T >
+void increment( T &x )
+{
+    x++;
+}
+
+template< typename T >
+void decrement( T &x )
+{
+    x--;
+}
+
+template< typename T >
+void append_a_char( T &x )
+{
+    x.append("a");
+}
+
+template< typename T >
+void iter (T *array, unsigned int length, void (*f)(T &param))
+{
+    for (unsigned int i = 0; i < length; i++)
+        f(array[i]);
+}
+
+#endif
--- CPP07/ex01/src/main.cpp
+++ CPP07/ex01/src/main.cpp
@@ -1,53 +1,103 @@
-#include <iostream>
-#include <Array.hpp>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cagonzal <cagonzal@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/30 14:57:53 by cagonzal          #+#    #+#             */
+/*   Updated: 2025/10/30 15:39:10 by cagonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#define MAX_VAL 750
-int main(int, char**)
+#include "iter.hpp"
+#include<iomanip>
+#include<iostream>
+
+
+int main( void ) 
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	
+	//Version Tester
+	int tab[] = { 0, 1, 2, 3, 4 };
+	Tester tab2[5];
+	::iter( tab, 5, print);
+	::iter( tab2, 5, print);	
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+	
+	// // Own version
+	int integer[5] = {1,2,3,4,5};
+	float floats[5] = {10.1f,20.1f,30.1f,40.1f,50.1f};
+	double doubles[5] = {0.01,1.01,2.01,3.01,4.01};
+	std::string strings[5] = {"Hola","que","tal","estas","?"};
+	
+	
+	try
+	{
+		std::cout << "Integer" << std::endl;
+		::iter( integer, 5 , &::print);
+		std::cout << "- - - add 1 - - - - -" << std::endl;
+		::iter( integer, 5 , &::increment);
+		::iter( integer, 5 , &::print);
+		// std::cout << "- - - rest 1 - - - - -" << std::endl;
+		// ::iter( integer, 5 , &::decrement);
+		// ::iter( integer, 5 , &::print);
+		std::cout << "___________________________" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Something happened"<< std::endl;
+	}
+	
+	try
+	{
+		std::cout << "Float" << std::endl;
+		::iter( floats, 5 , &::print);
+		std::cout << "- - - add 1 - - - - -" << std::endl;
+		::iter( floats, 5 , &::increment);
+		::iter( floats, 5 , &::print);
+		std::cout << "- - - rest 1 - - - - -" << std::endl;
+		::iter( floats, 5 , &::decrement);
+		::iter( floats, 5 , &::print);
+		std::cout << "___________________________" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Something happened"<< std::endl;
+	}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+	try
+	{
+		std::cout << "Doubles" << std::endl;
+		::iter( doubles, 5 , &::print);
+		std::cout << "- - - add 1 - - - - -" << std::endl;
+		::iter( doubles, 5 , &::increment);
+		::iter( doubles, 5 , &::print);
+		std::cout << "- - - rest 1 - - - - -" << std::endl;
+		::iter( doubles, 5 , &::decrement);
+		::iter( doubles, 5 , &::print);
+		std::cout << "___________________________" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Something happened"<< std::endl;
+	}
+
+	
+	try
+	{
+		std::cout << "strings" << std::endl;
+		::iter( strings, 5 , &::print);
+		std::cout << "- - - add a - - - - -" << std::endl;
+		::iter( strings, 5 , &::append_a_char);
+		::iter( strings, 5 , &::print);
+		std::cout << "___________________________" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Something happened"<< std::endl;
+	}
+
+	return 0; 
+
 }
